182d810baf8f9e54043614286722187ed95a0f45
Vladimirs Nordholm <v@vladde.net>
2022-01-07 04:32:14 +0100

    update pdf generators

--- source/generate_cards.ts
+++ source/generate_cards.ts
@@ -1,33 +1,31 @@
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
-import { presets } from ".";
 import * as path from 'path'
 import { LeshyCardGenerator } from "./generators/leshyCardGenerator";
-import { PixelProfilgateGenerator } from "./generators/pixelProfilgateCardGenerator";
 import { Card } from "./act1/types";
 
 const translations = JSON.parse(readFileSync('./translations.json', 'utf-8'))
-const locales = ['en'] // Object.keys(translations).filter(x => !['zh-cn', 'zh-tw', 'jp', 'ko'].includes(x))
 
-function generateCards(input: { [s: string]: Card }) {
-    for (const locale of locales) {
-        console.log(`Generating cards for locale '${locale}'`);
-        const cardGenerator = new PixelProfilgateGenerator()
+export function generateCards(input: { [s: string]: Card }, cardGenerator: LeshyCardGenerator, locale: string) {
 
-        const cardsPath = path.join('cards', locale)
+    for (const bordered of [true, false]) {
+
+        cardGenerator.setBorderedCards(bordered)
+
+        const cardsPath = path.join('cards', locale, bordered ? 'border' : 'regular')
         if (!existsSync(cardsPath)) {
             mkdirSync(cardsPath, { recursive: true })
         }
 
-        // const cardBacksPath = path.join(cardsPath, 'backs')
-        // if (!existsSync(cardBacksPath)) {
-        //     mkdirSync(cardBacksPath, { recursive: true })
-        // }
+        const cardBacksPath = path.join(cardsPath, 'backs')
+        if (!existsSync(cardBacksPath)) {
+            mkdirSync(cardBacksPath, { recursive: true })
+        }
 
-        // const backs: ('common' | 'squirrel' | 'bee' | 'deathcard')[] = ['common', 'squirrel', 'bee', 'deathcard']
-        // for (const back of backs) {
-        //     const buffer = cardGenerator.generateBack(back)
-        //     writeFileSync(path.join(cardBacksPath, back + '.png'), buffer)
-        // }
+        const backs: ('common' | 'squirrel' | 'bee' | 'deathcard')[] = ['common', 'squirrel', 'bee', 'deathcard']
+        for (const back of backs) {
+            const buffer = cardGenerator.generateBack(back)
+            writeFileSync(path.join(cardsPath, 'backs', back + '.png'), buffer)
+        }
 
         for (const id in input) {
 
@@ -38,82 +36,17 @@ function generateCards(input: { [s: string]: Card }) {
             const card = input[id]
             const name = card.name!;
 
-            const translatedName = translations[locale][name] as string
+            const translatedName = translations[locale][name] as string ?? name
 
             if (name && !translatedName) {
-                console.error(`ERROR: Missing translation for '${id}'`);
-                continue;
+                console.warn(`WARNING: Missing translation for '${id}'`);
             }
 
-            console.log(card)
-            const buffer = cardGenerator.generate({ ...card, name: translatedName })
-
-            // let filename = (translatedName ?? name).toLocaleLowerCase(locale).replace(/\s/g, '_')
-            // while (existsSync(path.join(cardsPath, filename + '.png'))) {
-            //     filename = `${filename}_x`
-            // }
+            const buffer = cardGenerator.generate({ ...card, name: translatedName }, locale)
 
             const filename = id;
 
             writeFileSync(path.join(cardsPath, filename + '.png'), buffer)
         }
-
-        writeFileSync(path.join(cardsPath, 'back.png'), cardGenerator.generateBack());
     }
 }
-
-const pixelProfilgateCards: { [s: string]: Card } = {
-    'adder': { ...presets['adder'], extra: { talkText: 'Always lethal, the caustic adder.' } },
-    'alpha': { ...presets['alpha'] },
-    'amalgam': { ...presets['amalgam'], extra: { talkText: 'Canine, reptile, bird, amalgam is all.' } },
-    'ant_queen': { ...presets['ant_queen'] },
-    'bat': { ...presets['bat'], extra: { talkText: 'A minor, meddlesome terror.' } },
-    'bee': { ...presets['bee'], extra: { talkText: 'Unassuming, but determined pests.' } },
-    'beehive': { ...presets['beehive'] },
-    'black_goat': { ...presets['black_goat'] },
-    'bloodhound': { ...presets['bloodhound'] },
-    'bullfrog': { ...presets['bullfrog'] },
-    'cat': { ...presets['cat'], extra: { talkText: 'It may not die, but id does feel the pain.' } },
-    'cockroach': { ...presets['cockroach'] },
-    'corpse_maggots': { ...presets['corpse_maggots'], extra: { talkText: 'Always inevitable, emerging from death.' } },
-    'coyote': { ...presets['coyote'], extra: { talkText: 'A meager, but dangerous, scavanger.' } },
-    'geck': { ...presets['geck'], extra: { talkText: 'Uninspiring, but charming despite it all' } },
-    'great_white': { ...presets['great_white'], extra: { talkText: 'Go fish.' } },
-    'grizzly': { ...presets['grizzly'], extra: { talkText: 'Its form speaks enough of its efficacy.' } },
-    'kingfisher': { ...presets['kingfisher'], extra: { talkText: 'A swift, slippery hunter.' } },
-    'magpie': { ...presets['magpie'] },
-    'mantis': { ...presets['mantis'], extra: { talkText: 'A slight incarnation of terror.' } },
-    'mantis_god': { ...presets['mantis_god'] },
-    'mole': { ...presets['mole'] },
-    'mole_man': { ...presets['mole_man'] },
-    'moose_buck': { ...presets['moose_buck'] },
-    'opossum': { ...presets['opossum'], extra: { talkText: 'Resourceful, weak, but endearing.' } },
-    'pack_rat': { ...presets['pack_rat'] },
-    'porcupine': { ...presets['porcupine'] },
-    'pronghorn': { ...presets['pronghorn'] },
-    'rabbit': { ...presets['rabbit'], extra: { talkText: 'What purpose could it serve?' } },
-    'rat_king': { ...presets['rat_king'] },
-    'rattler': { ...presets['rattler'], extra: { talkText: 'Brittle, if you can survive its bite.' } },
-    'raven': { ...presets['raven'], extra: { talkText: 'Horribly clever, this blight of a bird is.' } },
-    'raven_egg': { ...presets['raven_egg'] },
-    'river_otter': { ...presets['river_otter'], extra: { talkText: 'Elusive, but not very menacing.' } },
-    'river_snapper': { ...presets['river_snapper'], extra: { talkText: 'Stalward in the face of adversity.' } },
-    'skunk': { ...presets['skunk'], extra: { talkText: 'A horrid stench to keep predators away.' } },
-    'sparrow': { ...presets['sparrow'], extra: { talkText: 'Meek and feeble, yes. But ever so useful.' } },
-    'squirrel': { ...presets['squirrel'], extra: { talkText: 'Small, frail, but ever so useful.' } },
-    'turkey_vulture': { ...presets['turkey_vulture'], extra: { talkText: 'A horrid tyrant upon the skies.' } },
-    'urayuli': { ...presets['urayuli'], extra: { talkText: 'This level of brutality need no words.' } },
-    'warren': { ...presets['warren'] },
-    'wolf': { ...presets['wolf'], extra: { talkText: 'A proud, and vicious, contender.' } },
-    'wolf_cub': { ...presets['wolf_cub'] },
-    'worker_ant': { ...presets['worker_ant'], extra: { talkText: 'Weak alone, but overwhelming in force.' } },
-
-    'salmon': { type: 'common', name: 'salmon', portrait: 'salmon' as any, power: 2, health: 2, cost: { type: 'blood', amount: 2 }, sigils: ['submerge', 'strafe'] },
-    'squirrel_ball': { type: 'common', name: 'squirrelball', portrait: 'squirrelball' as any, power: 0, health: 1, cost: { type: 'blood', amount: 1 }, sigils: ['squirrelstrafe' as any] },
-    'stoat_talking': { type: 'common', name: 'stoat', portrait: 'stoat_talking' as any, power: 1, health: 3, cost: { type: 'blood', amount: 1 }, extra: { talkText: '"Yep, it\'s me. Nice topdeck."' } },
-    'stinkbug_talking': { type: 'common', name: 'stinkbug_talking', portrait: 'stinkbug_talking' as any, power: 1, health: 2, cost: { type: 'bone', amount: 2 }, sigils: ['debuffenemy'], extra: { talkText: '"Salutations, a lucky draw"' } },
-    'wolf_talking': { type: 'common', name: 'wolf_talking', portrait: 'wolf_talking' as any, power: 2, health: 2, cost: { type: 'blood', amount: 2 }, extra: { talkText: '"Careful now, use me wisely."' } },
-    'hrokkall': { type: 'rare', name: 'hrokkall', portrait: 'hrokkall' as any, power: 1, health: 1, sigils: ['submerge', 'gainbattery' as any], cost: { type: 'blood', amount: 1 }, extra: {talkText: 'Vicious in appearance, frail in nature.'} },
-}
-
-generateCards(pixelProfilgateCards)
--- source/generate_pdf.ts
+++ source/generate_pdf.ts
@@ -1,19 +1,133 @@
-// import { presets } from '.'
-
 import { execSync } from "child_process";
+import { existsSync, mkdirSync, readFileSync } from "fs";
 
-const lang = 'en'
+import { presets } from ".";
+import { generateCards } from "./generate_cards";
+import { LeshyCardGenerator } from "./generators/leshyCardGenerator";
+
+const translations = JSON.parse(readFileSync('./translations.json', 'utf-8'))
+const locales = Object.keys(translations)
 
 const cardIds: [string, string, string, string][] = [
-    ['wolf_cub','wolf','elk_fawn', 'elk'],
+    ['stoat_talking', 'stinkbug_talking', 'wolf_talking', 'blank'],
+    ['wolf_cub', 'wolf', 'elk_fawn', 'elk'],
+    ['raven_egg', 'raven', 'field_mice', 'beehive'],
+    ['adder', 'bullfrog', 'cat', 'river_snapper'],
+    ['mantis', 'beaver', 'mole', 'porcupine'],
+    ['pronghorn', 'river_otter', 'rat_king', 'ring_worm'],
+    ['ant_queen', 'worker_ant', 'skunk', 'skink'],
+    ['kingfisher', 'sparrow', 'magpie', 'bloodhound'],
+    ['squid_bell', 'squid_cards', 'squid_mirror', 'black_goat'],
+    ['alpha', 'coyote', 'opossum', 'rattler'],
+    ['corpse_maggots', 'cockroach', 'bat', 'turkey_vulture'],
+    ['warren', 'rabbit', 'rabbit', 'rabbit'],
+    ['tail_feathers', 'furry_tail', 'wriggling_leg', 'wriggling_tail'],
+    ['grizzly', 'great_white', 'moose_buck', 'urayuli'],
+
+    // rare
+    ['geck', 'mole_man', 'pack_rat', 'mantis_god'],
+    ['amalgam', 'amoeba', 'ouroboros', 'the_daus'],
+    ['strange_larva', 'strange_pupa', 'mothman', 'long_elk'],
+    ['child_13', 'child_13_awake', 'black_goat_sexy', 'pack_mule'],
+
+    // terrain
+    ['boulder', 'stump', 'grand_fir', 'snowy_fir'],
+    ['chime', 'chime', 'dam', 'dam'],
+    ['rabbit_pelt', 'wolf_pelt', 'golden_pelt', 'gold_nugget'],
+    ['frozen_opossum', 'leaping_trap', 'bait_bucket', 'strange_frog'],
+
+    // once only
+    ['hungry_child', 'leshy', 'caged_wolf', 'undead_cat'],
+    ['greater_smoke', 'the_smoke', 'starvation', 'starvation_flying'],
+
+    ['squirrel', 'squirrel', 'squirrel', 'squirrel'],
+    ['bee', 'bee', 'bee', 'bee'],
 ]
 
-const pathForId = (id: string, lang: string): string => {
-    return `./cards/${lang}/${id}.png`
+const pathForId = (id: string, locale: string): string => {
+    return `./cards/${locale}/border/${id}.png`
 }
 
-const backCommonPath = `./cards/${lang}/squirrel.png`;
+const backPathForId = (id: string, locale: string): string => {
+    switch (id) {
+        case 'squirrel':
+        case 'bee':
+            return `./cards/${locale}/border/backs/${id}.png`
+        case 'blank':
+            return `./cards/${locale}/border/backs/deathcard.png`
+        default: {
+            return `./cards/${locale}/border/backs/common.png`
+        }
+    }
+}
 
-const command = cardIds.map(ids => `convert -size 3507.9x2480.3 xc:white -fill rgb\\(33,28,32\\) -draw "rectangle 118.1,118.1 1561.4,1189.4" -draw "rectangle 118.1,1290.9 1561.4,2362.2" -draw "rectangle 1946.5,118.1 3389.8,1189.4" -draw "rectangle 1946.5,1290.9 3389.8,2362.2" -stroke rgb\\(128,128,128\\) -strokewidth 1 -draw "line 839.8,0 839.8,2480.3" -draw "line 2668.1,0 2668.1,2480.3" ${backCommonPath} -geometry +142.1+141.7 -composite ${backCommonPath} -geometry +142.1+1314.6 -composite ${backCommonPath} -geometry +1970.1+141.7 -composite ${backCommonPath} -geometry +1970.1+1314.6 -composite ${pathForId(ids[0], lang)} -geometry +863.4+141.7 -composite ${pathForId(ids[1], lang)} -geometry +863.4+1314.6 -composite ${pathForId(ids[2], lang)} -geometry +2691.7+141.7 -composite ${pathForId(ids[3], lang)} -geometry +2691.7+1314.6 -composite out.png`).join(' && ')
+const missingIds: string[] = []
+const cardIdsArray = cardIds.flat(2)
+for (const a of Object.keys(presets)) {
+    if (!cardIdsArray.includes(a)) {
+        missingIds.push(a)
+    }
+}
 
-execSync(command)
+if (missingIds.length) {
+    for (const m of missingIds) {
+        console.error('ERROR: Missing card', m)
+    }
+    process.exit(1)
+}
+
+if (!existsSync('./pdf/')) {
+    mkdirSync('./pdf/')
+}
+
+console.time('everything')
+for (const locale of locales) {
+
+    console.time('cards ' + locale)
+    generateCards(presets, new LeshyCardGenerator(), locale)
+    console.timeEnd('cards ' + locale)
+
+    // console.log('Creating unoptimized PDF');
+
+    const command = 'convert -density 300 ' + cardIds.map(ids => `
+\\(
+-size 3507.9x2480.3 xc:white -fill rgb\\(33,28,32\\)
+-draw "rectangle 118.1,118.1 1594.5,1215.3"
+-draw "rectangle 118.1,1265 1594.5,2362.2"
+-draw "rectangle 1913.4,118.1 3389.8,1215.3"
+-draw "rectangle 1913.4,1265 3389.8,2362.2"
+
+-stroke rgb\\(128,128,128\\)
+-strokewidth 1
+
+-draw "line 856.3,0 856.3,2480.3"
+-draw "line 2651.6,0 2651.6,2480.3"
+
+${backPathForId(ids[0], locale)} -geometry +142.1+141.7 -composite
+${backPathForId(ids[1], locale)} -geometry +142.1+1288.6 -composite
+${backPathForId(ids[2], locale)} -geometry +1937+141.7 -composite
+${backPathForId(ids[3], locale)} -geometry +1937+1288.6 -composite
+
+${pathForId(ids[0], locale)} -geometry +879.9+141.7 -composite
+${pathForId(ids[1], locale)} -geometry +879.9+1288.6 -composite
+${pathForId(ids[2], locale)} -geometry +2675.2+141.7 -composite
+${pathForId(ids[3], locale)} -geometry +2675.2+1288.6 -composite
+
+\\)`
+        .replace(/\n+/g, ' ')).join(' ') + ` pdf/${locale}.pdf`
+
+    console.time('pdf ' + locale)
+    execSync(command)
+    console.timeEnd('pdf ' + locale)
+
+    // optimiz the pdf. output is humungous!
+    // console.log('Optimizing PDF');
+
+    console.time('optimize ' + locale)
+    execSync(`ps2pdf -dPDFSETTINGS=/printer pdf/${locale}.pdf pdf/${locale}.optimized.pdf`)
+    execSync(`rm pdf/${locale}.pdf`)
+    execSync(`mv pdf/${locale}.optimized.pdf pdf/${locale}.pdf`)
+    console.timeEnd('optimize ' + locale)
+}
+
+console.timeEnd('everything')
