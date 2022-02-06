392463507fa5287b743a75618bd5e79e67bee784
Vladimirs Nordholm <v@vladde.net>
2022-02-06 11:43:33 +0100

    remove old files from v2

--- source/generators/cardGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { Card } from '../act1/types'
-
-interface CardGenerator {
-  generate(card: Card): Buffer;
-  generateBack(): Buffer;
-  requiredFiles?(): string[]
-}
-
-export { CardGenerator }
