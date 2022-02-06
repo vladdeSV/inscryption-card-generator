80a1eac88cce7308fa27cd778c0ba0a4bf81cf8a
Vladimirs Nordholm <v@vladde.net>
2022-02-06 16:18:28 +0100

    restructure card and json card

--- source/card.ts
+++ source/card.ts
@@ -1,42 +1,56 @@
-export { Card, CreatureId, Abillity, AppearanceBehaviour, Tribe, MetaCategory, Trait, Gem, StatIcon, Complexity, Temple, }
-
 interface Card {
-  id: CreatureId,
+  name: string,
+  portrait: Portrait,
+  cost: Cost
   power: number,
   health: number,
-  bloodCost: number,
-  boneCost: number,
-  energyCost: number,
-
-  temple: Temple,
-  complexity: Complexity,
-  metaCategories: MetaCategory[],
-  abilities: Abillity[],
-  appearanceBehaviours: AppearanceBehaviour[],
-  tribes: Tribe[],
-  traits: Trait[],
-  gems: Gem[]
-
-  sacrificable: boolean,
-  level: number,
-  oncePerDeck: boolean,
-  gemified: boolean,
-
-  statIcon: StatIcon | null,
-  iceCubeId?: string,
-  evolve?: {
-    turns: number,
-    evolvesTo: string,
+  sigils: Sigil[],
+  tribes: ('reptile' | 'canine' | 'bird' | 'hooved' | 'insect')[],
+  statIcon?: 'ants' | 'bones' | 'bell' | 'cardsinhand' | 'mirror' | 'greengems',
+  decals: ('snelk' | 'child' | 'leshy' | 'smoke' | 'combined')[],
+  flags: {
+    golden: boolean,
+    terrain: boolean,
   }
 }
 
-type CreatureId = 'Adder' | 'Alpha' | 'Amalgam' | 'Amoeba' | 'Ant' | 'AntQueen' | 'Bat' | 'Beaver' | 'Bee' | 'Beehive' | 'Bloodhound' | 'Bullfrog' | 'BurrowingTrap' | 'CagedWolf' | 'Cat' | 'CatUndead' | 'Cockroach' | 'Coyote' | 'Daus' | 'DefaultTail' | 'Elk' | 'ElkCub' | 'FieldMouse' | 'FieldMouse_Fused' | 'Geck' | 'Goat' | 'Grizzly' | 'Hawk' | 'Hrokkall' | 'JerseyDevil' | 'Kingfisher' | 'Kraken' | 'Lammergeier' | 'Maggots' | 'Magpie' | 'Mantis' | 'MantisGod' | 'Mole' | 'MoleMan' | 'Moose' | 'Mothman_Stage1' | 'Mothman_Stage2' | 'Mothman_Stage3' | 'Mule' | 'Opossum' | 'Otter' | 'Ouroboros' | 'PackRat' | 'Porcupine' | 'Pronghorn' | 'Rabbit' | 'RatKing' | 'Rattler' | 'Raven' | 'RavenEgg' | 'Salmon' | 'Shark' | 'Skink' | 'SkinkTail' | 'Skunk' | 'Snapper' | 'Snelk' | 'Sparrow' | 'SquidBell' | 'SquidCards' | 'SquidMirror' | 'Squirrel' | 'SquirrelBall' | 'Stoat' | 'Tail_Bird' | 'Tail_Furry' | 'Tail_Insect' | 'Urayuli' | 'Vulture' | 'Warren' | 'Wolf' | 'WolfCub' | '!DEATHCARD_BASE' | '!DEATHCARD_LESHY' | '!DEATHCARD_VICTORY' | '!GIANTCARD_MOON' | '!STATIC!GLITCH' | 'BaitBucket' | 'CardMergeStones' | 'Dam' | 'DausBell' | 'GoldNugget' | 'PeltGolden' | 'PeltHare' | 'PeltWolf' | 'RingWorm' | 'Smoke' | 'Smoke_Improved' | 'Smoke_NoBones' | 'Starvation' | 'Stinkbug_Talking' | 'Stoat_Talking' | 'Trap' | 'TrapFrog' | 'Wolf_Talking' | '!CORRUPTED' | '!DEATHCARD_PIXEL_BASE' | '!INSPECTOR' | '!MELTER' | '!BOUNTYHUNTER_BASE' | '!BUILDACARD_BASE' | '!FRIENDCARD_BASE' | '!MYCO_OLD_DATA' | '!MYCOCARD_BASE' | 'Angler_Fish_Bad' | 'Angler_Fish_Good' | 'Angler_Fish_More' | 'Angler_Talking' | 'BlueMage_Talking' | 'DUMMY_5-5' | 'Mole_Telegrapher' | 'Mummy_Telegrapher' | 'Ouroboros_Part3' | 'AboveCurve' | 'AlarmBot' | 'Amoebot' | 'AttackConduit' | 'Automaton' | 'BatteryBot' | 'BoltHound' | 'Bombbot' | 'BombMaiden' | 'BustedPrinter' | 'CaptiveFile' | 'CellBuff' | 'CellGift' | 'CellTri' | 'CloserBot' | 'CXformerAdder' | 'CXformerElk' | 'CXformerRaven' | 'CXformerWolf' | 'EmptyVessel' | 'EmptyVessel_BlueGem' | 'EmptyVessel_GreenGem' | 'EmptyVessel_OrangeGem' | 'EnergyConduit' | 'EnergyRoller' | 'FactoryConduit' | 'GemExploder' | 'GemRipper' | 'GemsConduit' | 'GemShielder' | 'GiftBot' | 'HealerConduit' | 'Insectodrone' | 'LatcherBomb' | 'LatcherBrittle' | 'LatcherShield' | 'LeapBot' | 'Librarian' | 'MeatBot' | 'MineCart' | 'NullConduit' | 'PlasmaGunner' | 'RoboMice' | 'RoboSkeleton' | 'SentinelBlue' | 'SentinelGreen' | 'SentinelOrange' | 'SentryBot' | 'SentryBot_Fused' | 'Shieldbot' | 'Shutterbug' | 'Sniper' | 'Steambot' | 'SwapBot' | 'TechMoxTriple' | 'Thickbot' | 'XformerBatBeast' | 'XformerBatBot' | 'XformerGrizzlyBeast' | 'XformerGrizzlyBot' | 'XformerPorcupineBeast' | 'XformerPorcupineBot' | 'AnnoyTower' | 'Boulder' | 'BridgeRailing' | 'BrokenBot' | 'ConduitTower' | 'DeadTree' | 'FrozenOpossum' | 'Stump' | 'TombStone' | 'Tree' | 'Tree_Hologram' | 'Tree_Hologram_SnowCovered' | 'Tree_SnowCovered' | 'Banshee' | 'Bonehound' | 'BonelordHorn' | 'Bonepile' | 'CoinLeft' | 'CoinRight' | 'DeadHand' | 'DeadPets' | 'Draugr' | 'DrownedSoul' | 'Family' | 'FrankNStein' | 'GhostShip' | 'Gravedigger' | 'Gravedigger_Fused' | 'HeadlessHorseman' | 'Mummy' | 'Necromancer' | 'Revenant' | 'Sarcophagus' | 'Skeleton' | 'SkeletonMage' | 'TombRobber' | 'Zombie' | 'BlueMage' | 'BlueMage_Fused' | 'FlyingMage' | 'ForceMage' | 'GemFiend' | 'GreenMage' | 'JuniorSage' | 'MageKnight' | 'MarrowMage' | 'MasterBleene' | 'MasterGoranj' | 'MasterOrlu' | 'MoxDualBG' | 'MoxDualGO' | 'MoxDualOB' | 'MoxEmerald' | 'MoxRuby' | 'MoxSapphire' | 'MoxTriple' | 'MuscleMage' | 'OrangeMage' | 'PracticeMage' | 'PracticeMageSmall' | 'Pupil' | 'RubyGolem' | 'StimMage'
-type Abillity = 'Deathtouch' | 'BuffNeighbours' | 'RandomAbility' | 'Ant' | 'DrawAnt' | 'Flying' | 'CreateDams' | 'BeesOnHit' | 'GuardDog' | 'Reach' | 'WhackAMole' | 'SteelTrap' | 'CagedWolf' | 'Sacrificial' | 'Cat' | 'DrawCopyOnDeath' | 'CreateBells' | 'Daus' | 'Strafe' | 'Evolve' | 'DrawCopy' | 'TripleBlood' | 'Submerge' | 'GainBattery' | 'JerseyDevil' | 'SubmergeSquid' | 'Lammergeier' | 'CorpseEater' | 'Tutor' | 'SplitStrike' | 'TriStrike' | 'StrafePush' | 'PackMule' | 'Ouroboros' | 'RandomConsumable' | 'Sharp' | 'QuadrupleBones' | 'TailOnHit' | 'DebuffEnemy' | 'BellProximity' | 'CardsInHand' | 'Mirror' | 'SquirrelStrafe' | 'DrawRabbits' | 'AllStrike' | 'SquirrelOrbit' | 'GiantCard' | 'GiantMoon' | 'RandomCard' | 'PreventAttack' | 'TalkingCardChooser' | 'TrapSpawner' | 'ConduitNull' | 'IceCube' | 'BountyHunter' | 'Brittle' | 'BuffEnemy' | 'Sentry' | 'Sniper' | 'DrawRandomCardOnDeath' | 'MoveBeside' | 'ConduitBuffAttack' | 'ExplodeOnDeath' | 'BombSpawner' | 'DrawVesselOnHit' | 'DeleteFile' | 'CellBuffSelf' | 'CellDrawRandomCardOnDeath' | 'CellTriStrike' | 'GainGemBlue' | 'GainGemGreen' | 'GainGemOrange' | 'ConduitEnergy' | 'ActivatedRandomPowerEnergy' | 'ConduitFactory' | 'ExplodeGems' | 'ConduitSpawnGems' | 'ShieldGems' | 'ConduitHeal' | 'LatchExplodeOnDeath' | 'LatchBrittle' | 'LatchDeathShield' | 'FileSizeDamage' | 'ActivatedDealDamage' | 'DeathShield' | 'SwapStats' | 'GainGemTriple' | 'Transformer' | 'ActivatedEnergyToBones' | 'ActivatedStatsUp' | 'BrokenCoinLeft' | 'BrokenCoinRight' | 'DrawNewHand' | 'SkeletonStrafe' | 'BoneDigger' | 'DoubleDeath' | 'GemDependant' | 'ActivatedDrawSkeleton' | 'GemsDraw' | 'GreenMage' | 'ActivatedSacrificeDrawCards' | 'Loot' | 'BuffGems' | 'DropRubyOnDeath' | 'ActivatedStatsUpEnergy'
-type AppearanceBehaviour = 'RareCardBackground' | 'TerrainBackground' | 'TerrainLayout' | 'SexyGoat' | 'AlternatingBloodDecal' | 'AddSnelkDecals' | 'DynamicPortrait' | 'GiantAnimatedPortrait' | 'StaticGlitch' | 'GoldEmission' | 'AnimatedPortrait' | 'HologramPortrait'
-type Tribe = 'Reptile' | 'Canine' | 'Bird' | 'Hooved' | 'Insect' | 'Squirrel'
-type MetaCategory = 'ChoiceNode' | 'TraderOffer' | 'GBCPack' | 'GBCPlayable' | 'Rare' | 'Part3Random'
-type Trait = 'KillsSurvivors' | 'Ant' | 'Blind' | 'Structure' | 'Terrain' | 'DeathcardCreationNonOption' | 'Undead' | 'FeedsStoat' | 'Fused' | 'Goat' | 'Uncuttable' | 'SatisfiesRingTrial' | 'Wolf' | 'Giant' | 'Pelt' | 'Gem'
-type Gem = 'Blue' | 'Green' | 'Orange'
-type StatIcon = 'Ants' | 'Bones' | 'Bell' | 'CardsInHand' | 'Mirror' | 'GreenGems'
-type Complexity = 'Simple' | 'Intermediate' | 'Advanced' | 'Vanilla'
-type Temple = 'Nature' | 'Tech' | 'Undead' | 'Wizard'
+type Portrait = CreaturePortrait | DeathcardPortrait | CustomPortrait
+type CreaturePortrait = {
+  type: 'creature',
+  id: CreatureId
+}
+type DeathcardPortrait = {
+  type: 'deathcard',
+  data: {
+    headType: 'chief' | 'enchantress' | 'gravedigger' | 'prospector' | 'robot' | 'settlerman' | 'settlerwoman' | 'wildling',
+    mouthIndex: number, // 0 - 5
+    eyesIndex: number, // 0 - 5
+    lostEye: boolean,
+  }
+}
+type CustomPortrait = {
+  type: 'custom',
+  data: Buffer,
+}
+
+type Cost = BloodCost | BoneCost | EnergyCost | GemCost
+type BloodCost = {
+  type: 'blood',
+  amount: number
+}
+type BoneCost = {
+  type: 'bone',
+  amount: number
+}
+type EnergyCost = {
+  type: 'energy',
+  amount: number
+}
+type GemCost = {
+  type: 'gem',
+  games: ('blue' | 'green' | 'orange')[]
+}
+
+type Sigil = 'deathtouch' | 'buffneighbours' | 'randomability' | 'ant' | 'drawant' | 'flying' | 'createdams' | 'beesonhit' | 'guarddog' | 'reach' | 'whackamole' | 'steeltrap' | 'cagedwolf' | 'sacrificial' | 'cat' | 'drawcopyondeath' | 'createbells' | 'daus' | 'strafe' | 'evolve' | 'drawcopy' | 'tripleblood' | 'submerge' | 'gainbattery' | 'jerseydevil' | 'submergesquid' | 'lammergeier' | 'corpseeater' | 'tutor' | 'splitstrike' | 'tristrike' | 'strafepush' | 'packmule' | 'ouroboros' | 'randomconsumable' | 'sharp' | 'quadruplebones' | 'tailonhit' | 'debuffenemy' | 'bellproximity' | 'cardsinhand' | 'mirror' | 'squirrelstrafe' | 'drawrabbits' | 'allstrike' | 'squirrelorbit' | 'giantcard' | 'giantmoon' | 'randomcard' | 'preventattack' | 'talkingcardchooser' | 'trapspawner' | 'conduitnull' | 'icecube' | 'bountyhunter' | 'brittle' | 'buffenemy' | 'sentry' | 'sniper' | 'drawrandomcardondeath' | 'movebeside' | 'conduitbuffattack' | 'explodeondeath' | 'bombspawner' | 'drawvesselonhit' | 'deletefile' | 'cellbuffself' | 'celldrawrandomcardondeath' | 'celltristrike' | 'gaingemblue' | 'gaingemgreen' | 'gaingemorange' | 'conduitenergy' | 'activatedrandompowerenergy' | 'conduitfactory' | 'explodegems' | 'conduitspawngems' | 'shieldgems' | 'conduitheal' | 'latchexplodeondeath' | 'latchbrittle' | 'latchdeathshield' | 'filesizedamage' | 'activateddealdamage' | 'deathshield' | 'swapstats' | 'gaingemtriple' | 'transformer' | 'activatedenergytobones' | 'activatedstatsup' | 'brokencoinleft' | 'brokencoinright' | 'drawnewhand' | 'skeletonstrafe' | 'bonedigger' | 'doubledeath' | 'gemdependant' | 'activateddrawskeleton' | 'gemsdraw' | 'greenmage' | 'activatedsacrificedrawcards' | 'loot' | 'buffgems' | 'droprubyondeath' | 'activatedstatsupenergy'
+
+type CreatureId = 'adder' | 'alpha' | 'amalgam' | 'amoeba' | 'ant' | 'antqueen' | 'bat' | 'beaver' | 'bee' | 'beehive' | 'bloodhound' | 'bullfrog' | 'burrowingtrap' | 'cagedwolf' | 'cat' | 'catundead' | 'cockroach' | 'coyote' | 'daus' | 'defaulttail' | 'elk' | 'elkcub' | 'fieldmouse' | 'fieldmouse_fused' | 'geck' | 'goat' | 'grizzly' | 'hawk' | 'hrokkall' | 'jerseydevil' | 'kingfisher' | 'kraken' | 'lammergeier' | 'maggots' | 'magpie' | 'mantis' | 'mantisgod' | 'mole' | 'moleman' | 'moose' | 'mothman_stage1' | 'mothman_stage2' | 'mothman_stage3' | 'mule' | 'opossum' | 'otter' | 'ouroboros' | 'packrat' | 'porcupine' | 'pronghorn' | 'rabbit' | 'ratking' | 'rattler' | 'raven' | 'ravenegg' | 'salmon' | 'shark' | 'skink' | 'skinktail' | 'skunk' | 'snapper' | 'snelk' | 'sparrow' | 'squidbell' | 'squidcards' | 'squidmirror' | 'squirrel' | 'squirrelball' | 'stoat' | 'tail_bird' | 'tail_furry' | 'tail_insect' | 'urayuli' | 'vulture' | 'warren' | 'wolf' | 'wolfcub' | '!deathcard_base' | '!deathcard_leshy' | '!deathcard_victory' | '!giantcard_moon' | '!static!glitch' | 'baitbucket' | 'cardmergestones' | 'dam' | 'dausbell' | 'goldnugget' | 'peltgolden' | 'pelthare' | 'peltwolf' | 'ringworm' | 'smoke' | 'smoke_improved' | 'smoke_nobones' | 'starvation' | 'stinkbug_talking' | 'stoat_talking' | 'trap' | 'trapfrog' | 'wolf_talking' | '!corrupted' | '!deathcard_pixel_base' | '!inspector' | '!melter' | '!bountyhunter_base' | '!buildacard_base' | '!friendcard_base' | '!myco_old_data' | '!mycocard_base' | 'angler_fish_bad' | 'angler_fish_good' | 'angler_fish_more' | 'angler_talking' | 'bluemage_talking' | 'dummy_5-5' | 'mole_telegrapher' | 'mummy_telegrapher' | 'ouroboros_part3' | 'abovecurve' | 'alarmbot' | 'amoebot' | 'attackconduit' | 'automaton' | 'batterybot' | 'bolthound' | 'bombbot' | 'bombmaiden' | 'bustedprinter' | 'captivefile' | 'cellbuff' | 'cellgift' | 'celltri' | 'closerbot' | 'cxformeradder' | 'cxformerelk' | 'cxformerraven' | 'cxformerwolf' | 'emptyvessel' | 'emptyvessel_bluegem' | 'emptyvessel_greengem' | 'emptyvessel_orangegem' | 'energyconduit' | 'energyroller' | 'factoryconduit' | 'gemexploder' | 'gemripper' | 'gemsconduit' | 'gemshielder' | 'giftbot' | 'healerconduit' | 'insectodrone' | 'latcherbomb' | 'latcherbrittle' | 'latchershield' | 'leapbot' | 'librarian' | 'meatbot' | 'minecart' | 'nullconduit' | 'plasmagunner' | 'robomice' | 'roboskeleton' | 'sentinelblue' | 'sentinelgreen' | 'sentinelorange' | 'sentrybot' | 'sentrybot_fused' | 'shieldbot' | 'shutterbug' | 'sniper' | 'steambot' | 'swapbot' | 'techmoxtriple' | 'thickbot' | 'xformerbatbeast' | 'xformerbatbot' | 'xformergrizzlybeast' | 'xformergrizzlybot' | 'xformerporcupinebeast' | 'xformerporcupinebot' | 'annoytower' | 'boulder' | 'bridgerailing' | 'brokenbot' | 'conduittower' | 'deadtree' | 'frozenopossum' | 'stump' | 'tombstone' | 'tree' | 'tree_hologram' | 'tree_hologram_snowcovered' | 'tree_snowcovered' | 'banshee' | 'bonehound' | 'bonelordhorn' | 'bonepile' | 'coinleft' | 'coinright' | 'deadhand' | 'deadpets' | 'draugr' | 'drownedsoul' | 'family' | 'franknstein' | 'ghostship' | 'gravedigger' | 'gravedigger_fused' | 'headlesshorseman' | 'mummy' | 'necromancer' | 'revenant' | 'sarcophagus' | 'skeleton' | 'skeletonmage' | 'tombrobber' | 'zombie' | 'bluemage' | 'bluemage_fused' | 'flyingmage' | 'forcemage' | 'gemfiend' | 'greenmage' | 'juniorsage' | 'mageknight' | 'marrowmage' | 'masterbleene' | 'mastergoranj' | 'masterorlu' | 'moxdualbg' | 'moxdualgo' | 'moxdualob' | 'moxemerald' | 'moxruby' | 'moxsapphire' | 'moxtriple' | 'musclemage' | 'orangemage' | 'practicemage' | 'practicemagesmall' | 'pupil' | 'rubygolem' | 'stimmage'
