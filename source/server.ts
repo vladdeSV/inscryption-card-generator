015e6a8b1c4288f9ada3100d7708552c89467f75
Vladimirs Nordholm <v@vladde.net>
2022-03-17 23:19:47 +0100

    temp: close server after 10 seconds

--- source/server.ts
+++ source/server.ts
@@ -224,4 +224,9 @@ server.post('/api/card/:id/', (request, reply) => {
 })
 
 server.get('/', (_, reply) => reply.status(200).send('OK'))
-server.listen(8080, () => console.log('Server running'))
+const a = server.listen(8080, () => {
+  console.log('Server running')
+  setTimeout(() => {
+    a.close()
+  }, 10000)
+})
