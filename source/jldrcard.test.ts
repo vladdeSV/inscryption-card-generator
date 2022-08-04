5630665b195e1b3e374e4e2b39449ea5a5717115
Vladimirs Nordholm <v@vladde.net>
2022-08-04 03:26:23 +0200

    fix: tests for jldr card include metacategories

--- source/jldrcard.test.ts
+++ source/jldrcard.test.ts
@@ -5,6 +5,8 @@ import { act1Resource } from './generators/leshyCardGenerator'
 import { convert, createResourcesForCard, JldrCreature } from './jldrcard'
 import { res2 } from './temp'
 
+const defaultMetaCategories = ['ChoiceNode', 'TraderOffer', 'GBCPack', 'GBCPlayable']
+
 const templateCard: Card = {
   name: '',
   cost: undefined,
@@ -34,134 +36,134 @@ const templateCard: Card = {
 describe('convert simple cards', () => {
   test('empty card', () => {
     expect(convert({ ...templateCard }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
   })
 
   test('display name', () => {
     expect(convert({ ...templateCard, name: 'Foo Bar' }, 'test'))
-      .toEqual({ name: 'test', displayedName: 'Foo Bar' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, displayedName: 'Foo Bar' } as JldrCreature)
 
     expect(convert({ ...templateCard, name: '' }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
   })
 
   test('power', () => {
     expect(convert({ ...templateCard, power: 0 }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, power: 1 }, 'test'))
-      .toEqual({ name: 'test', baseAttack: 1 } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, baseAttack: 1 } as JldrCreature)
   })
 
   test('health', () => {
     expect(convert({ ...templateCard, health: 0 }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, health: 1 }, 'test'))
-      .toEqual({ name: 'test', baseHealth: 1 } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, baseHealth: 1 } as JldrCreature)
 
     expect(convert({ ...templateCard, health: 4 }, 'test'))
-      .toEqual({ name: 'test', baseHealth: 4 } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, baseHealth: 4 } as JldrCreature)
   })
 
   test('blood cost', () => {
     const card = convert({ ...templateCard, cost: { type: 'blood', amount: 1, } }, 'test')
-    expect(card).toEqual({ name: 'test', bloodCost: 1, } as JldrCreature)
+    expect(card).toEqual({ name: 'test', metaCategories: defaultMetaCategories, bloodCost: 1, } as JldrCreature)
   })
 
   test('bone cost', () => {
     const card = convert({ ...templateCard, cost: { type: 'bone', amount: 1, } }, 'test')
-    expect(card).toEqual({ name: 'test', bonesCost: 1, } as JldrCreature)
+    expect(card).toEqual({ name: 'test', metaCategories: defaultMetaCategories, bonesCost: 1, } as JldrCreature)
   })
 
   test('energy cost', () => {
     const card = convert({ ...templateCard, cost: { type: 'energy', amount: 1, } }, 'test')
-    expect(card).toEqual({ name: 'test', energyCost: 1, } as JldrCreature)
+    expect(card).toEqual({ name: 'test', metaCategories: defaultMetaCategories, energyCost: 1, } as JldrCreature)
   })
 
   test('tribes', () => {
     expect(convert({ ...templateCard, tribes: [] }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, tribes: ['canine'] }, 'test'))
-      .toEqual({ name: 'test', tribes: ['Canine'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, tribes: ['Canine'] } as JldrCreature)
 
     expect(convert({ ...templateCard, tribes: ['squirrel', 'reptile', 'canine', 'bird', 'hooved', 'insect'] }, 'test'))
-      .toEqual({ name: 'test', tribes: ['Squirrel', 'Reptile', 'Canine', 'Bird', 'Hooved', 'Insect',] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, tribes: ['Squirrel', 'Reptile', 'Canine', 'Bird', 'Hooved', 'Insect',] } as JldrCreature)
   })
 
   test('temple', () => {
     expect(convert({ ...templateCard, temple: 'nature' }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, temple: 'tech' }, 'test'))
-      .toEqual({ name: 'test', temple: 'Tech' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, temple: 'Tech' } as JldrCreature)
   })
 
   test('rare', () => {
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, rare: false } }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, rare: true } }, 'test'))
-      .toEqual({ name: 'test', metaCategories: ['Rare'], appearanceBehaviour: ['RareCardBackground'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: [...defaultMetaCategories, 'Rare'], appearanceBehaviour: ['RareCardBackground'] } as JldrCreature)
   })
 
   test('terrain', () => {
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, terrain: false, terrainLayout: false } }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, terrain: true, terrainLayout: false } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['TerrainBackground'], traits: ['Terrain'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['TerrainBackground'], traits: ['Terrain'] } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, terrain: false, terrainLayout: true } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['TerrainLayout'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['TerrainLayout'] } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, terrain: true, terrainLayout: true } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['TerrainBackground', 'TerrainLayout'], traits: ['Terrain'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['TerrainBackground', 'TerrainLayout'], traits: ['Terrain'] } as JldrCreature)
   })
 
   test('stat icons', () => {
 
     expect(convert({ ...templateCard, statIcon: undefined }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: undefined, power: 2 }, 'test'))
-      .toEqual({ name: 'test', baseAttack: 2 } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, baseAttack: 2 } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'ants' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'Ants', specialAbilities: ['Ant'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'Ants', specialAbilities: ['Ant'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'bell' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'Bell', specialAbilities: ['BellProximity'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'Bell', specialAbilities: ['BellProximity'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'bones' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'Bones', specialAbilities: ['Lammergeier'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'Bones', specialAbilities: ['Lammergeier'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'cardsinhand' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'CardsInHand', specialAbilities: ['CardsInHand'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'CardsInHand', specialAbilities: ['CardsInHand'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'greengems' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'GreenGems', specialAbilities: ['GreenMage'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'GreenGems', specialAbilities: ['GreenMage'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'mirror' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'Mirror', specialAbilities: ['Mirror'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'Mirror', specialAbilities: ['Mirror'] } as JldrCreature)
 
     expect(convert({ ...templateCard, statIcon: 'sacrificesthisturn' }, 'test'))
-      .toEqual({ name: 'test', specialStatIcon: 'SacrificesThisTurn', specialAbilities: ['SacrificesThisTurn'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, specialStatIcon: 'SacrificesThisTurn', specialAbilities: ['SacrificesThisTurn'] } as JldrCreature)
   })
 
   test('misc', () => {
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, blood: true } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['AlternatingBloodDecal'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['AlternatingBloodDecal'] } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, golden: true } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['GoldEmission'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['GoldEmission'] } as JldrCreature)
 
     expect(convert({ ...templateCard, flags: { ...templateCard.flags, redEmission: true } }, 'test'))
-      .toEqual({ name: 'test', appearanceBehaviour: ['RedEmission'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, appearanceBehaviour: ['RedEmission'] } as JldrCreature)
   })
 
   test('fused', () => {
@@ -172,13 +174,13 @@ describe('convert simple cards', () => {
 
   test('sigils', () => {
     expect(convert({ ...templateCard, sigils: [] }, 'test'))
-      .toEqual({ name: 'test' } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories } as JldrCreature)
 
     expect(convert({ ...templateCard, sigils: ['reach'] }, 'test'))
-      .toEqual({ name: 'test', abilities: ['Reach'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, abilities: ['Reach'] } as JldrCreature)
 
     expect(convert({ ...templateCard, sigils: ['sharp', 'brittle'] }, 'test'))
-      .toEqual({ name: 'test', abilities: ['Sharp', 'Brittle'] } as JldrCreature)
+      .toEqual({ name: 'test', metaCategories: defaultMetaCategories, abilities: ['Sharp', 'Brittle'] } as JldrCreature)
   })
 
   test('additional textures', () => {
@@ -205,6 +207,7 @@ describe('convert simple cards', () => {
       pixelTexture: 'test123_pixel_portrait.png',
       decals: ['test123_paint.png', 'test123_smoke.png'],
       titleGraphic: 'test123_squid.png',
+      metaCategories: ['ChoiceNode', 'TraderOffer', 'GBCPack', 'GBCPlayable']
     }
 
     expect(convert(card, 'test123')).toEqual(out)
